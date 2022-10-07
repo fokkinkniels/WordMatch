cafdde98e7252ebb9ca418087a0e5c1d8c91930a
fokkinkniels <60216993+fokkinkniels@users.noreply.github.com>
2022-10-07 11:06:47 +0200

    Created Friend Service

---
+++ WoordMatch_FriendService/src/main/java/com/example/woordmatch_friendservice/WoordMatchFriendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.woordmatch_friendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoordMatchFriendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoordMatchFriendServiceApplication.class, args);
+	}
+
+}
---
+++ WoordMatch_FriendService/src/test/java/com/example/woordmatch_friendservice/WoordMatchFriendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.woordmatch_friendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoordMatchFriendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
