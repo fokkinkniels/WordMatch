501c1556a26f928c1dd73dd00915ce88b2317a5e
fokkinkniels <60216993+fokkinkniels@users.noreply.github.com>
2022-11-07 22:04:55 +0100

    implementing socketio rooms and private messaging
    still WIP
    pages dont work this build

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,66 @@
 <template>
-    <div> 
-      <User/>
+    <div id="app"> 
+      <select-username
+        v-if="!usernameAlreadySelected"
+        @input="onUsernameSelection"
+      />
+    <chat v-else />
   </div>
 </template>
 
 
 <script>
-import User from './components/User.vue'
+
+import SelectUsername from "./components/SelectUsername";
+import Chat from "./components/Chat";
+import socket from "./socket";
 
 export default {
     name: "App",
     components: {
-        User,
+        Chat,
+        SelectUsername,
+    },
+    data() {
+      return {
+        usernameAlreadySelected: false,
+      };
+  },
+  methods: {
+      onUsernameSelection(username) {
+        //implement own login system and own usernames
+        this.usernameAlreadySelected = true;
+        socket.auth = { username };
+        socket.connect();
+      },
+  },
+  created() {
+    const sessionID = localStorage.getItem("sessionID");
+
+    if (sessionID) {
+      this.usernameAlreadySelected = true;
+      socket.auth = { sessionID };
+      socket.connect();
     }
+
+    socket.on("session", ({ sessionID, userID }) => {
+      // attach the session ID to the next reconnection attempts
+      socket.auth = { sessionID };
+      // store it in the localStorage
+      localStorage.setItem("sessionID", sessionID);
+      // save the ID of the user
+      socket.userID = userID;
+    });
+
+    socket.on("connect_error", (err) => {
+      if (err.message === "invalid username") {
+        this.usernameAlreadySelected = false;
+      }
+    });
+  },
+  unmounted() {
+    socket.off("connect_error");
+  },
 }
 
 </script>
