58b32e1049b073fa15da7b04ae718fd6a63e8e2d
fokkinkniels <fokkinkniels@gmail.com>
2022-11-10 14:09:05 +0100

    Revert "implementing socketio rooms and private messaging"
    This reverts commit 501c1556a26f928c1dd73dd00915ce88b2317a5e.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,66 +1,18 @@
 <template>
-    <div id="app"> 
-      <select-username
-        v-if="!usernameAlreadySelected"
-        @input="onUsernameSelection"
-      />
-    <chat v-else />
+    <div> 
+      <User/>
   </div>
 </template>
 
 
 <script>
-
-import SelectUsername from "./components/SelectUsername";
-import Chat from "./components/Chat";
-import socket from "./socket";
+import User from './components/User.vue'
 
 export default {
     name: "App",
     components: {
-        Chat,
-        SelectUsername,
-    },
-    data() {
-      return {
-        usernameAlreadySelected: false,
-      };
-  },
-  methods: {
-      onUsernameSelection(username) {
-        //implement own login system and own usernames
-        this.usernameAlreadySelected = true;
-        socket.auth = { username };
-        socket.connect();
-      },
-  },
-  created() {
-    const sessionID = localStorage.getItem("sessionID");
-
-    if (sessionID) {
-      this.usernameAlreadySelected = true;
-      socket.auth = { sessionID };
-      socket.connect();
+        User,
     }
-
-    socket.on("session", ({ sessionID, userID }) => {
-      // attach the session ID to the next reconnection attempts
-      socket.auth = { sessionID };
-      // store it in the localStorage
-      localStorage.setItem("sessionID", sessionID);
-      // save the ID of the user
-      socket.userID = userID;
-    });
-
-    socket.on("connect_error", (err) => {
-      if (err.message === "invalid username") {
-        this.usernameAlreadySelected = false;
-      }
-    });
-  },
-  unmounted() {
-    socket.off("connect_error");
-  },
 }
 
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import vue3GoogleLogin from 'vue3-google-login'
 
 const app = createApp(App)
 
@@ -12,6 +13,10 @@ app.config.globalProperties.$id = ""
 axios.defaults.baseURL = process.env.VUE_APP_BASE_URL_API
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.put['Content-Type'] = 'application/json';
+
+app.use(vue3GoogleLogin, {
+  clientId: '937641477436-alps1oc5b96kplmck0gr3v2ts7tu8qao.apps.googleusercontent.com'
+})
 
 
 app.mount('#app')
